c2c5a9fc34c985ae1b7a37fb7a39f8f07c0a57e8
Sheikh Umar <shumarb@outlook.com>
2023-12-30 07:59:52 +0800

    Added databasewriter java file

---
+++ fdm/object-oriented-development/part-2/tdd-assessment-submission/src/main/java/com/fdmgroup/tdd/groupcontroller/DatabaseWriter.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.tdd.groupcontroller;
+
+public class DatabaseWriter {
+
+	public void addTrainee(Trainee any) {
+		
+	}
+
+	public void deleteTraineeByUsername(String anyString) {
+		
+	}
+	
+}
