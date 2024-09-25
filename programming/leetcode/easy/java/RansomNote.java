e174d4fa5b6d38db1a5ffa38ce90cee364798efa
S.Umar <shumarb@outlook.com>
2024-09-25 09:17:29 +0800

    Added solution to exercise on Ransom Note with improved performance

--- programming/leetcode/easy/java/RansomNote.java
+++ programming/leetcode/easy/java/RansomNote.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<String, Integer> map = new HashMap<> ();
-        for (int i = 0; i < magazine.length(); i++) {
-            String letter = Character.toString(magazine.charAt(i));
-            if (!map.containsKey(letter)) {
-                map.put(letter, 1);
-            } else {
-                map.put(letter, 1 + map.get(letter));
-            }
+        Map<Character, Integer> map = new HashMap<> ();
+
+        for (char c: magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for (int i = 0; i < ransomNote.length(); i++) {
-            String letter = Character.toString(ransomNote.charAt(i));
-            if (!map.containsKey(letter)) {
+        for (char c: ransomNote.toCharArray()) {
+            // If a character c in ransom note is not present in magazine,
+            // or character is present but number of characters available to use is 0
+            // the ransom note can't be formed frmo magazine
+            if (!map.containsKey(c) || map.get(c) <= 0) {
                 return false;
-            } else {
-                if (map.get(letter) == 0) {
-                    return false;
-                } else {
-                    map.put(letter, map.get(letter) - 1);
-                }
             }
+
+            // A character c is in magazine
+            // and can be used to form a ransom note, so reduce number of available characters c
+            // in magazine by 1
+            map.put(c, map.get(c) - 1);
         }
+
         return true;
     }
 }
